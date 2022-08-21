8923a2df3a8f4ea9e8ad10e5dc027bc02992f238
arman <mr.kazemi.ar@gmail.com>
2022-08-21 11:26:50 +0430

    style(train): Rename checkpoints to save_dir
    Closes #4

--- train_app.py
+++ train_app.py
@@ -6,13 +6,13 @@ app = FastAPI(title="OCR API", description="OCR API this package developed for t
 
 
 @app.post("/train")
-async def train(img_w: int, img_h: int, labels_txt_p: str, images_path: str, checkpoint_path: str, model_name: str,
+async def train(img_w: int, img_h: int, labels_txt_p: str, images_path: str, save_dir: str, model_name: str,
                 max_epochs: int = 1000, lr: float = 1e-3, batch_size: int = 128, log_url: str = None,
                 task_id: str = None):
     ocr_trainer(img_w=img_w, img_h=img_h,
                 labels_txt_p=labels_txt_p,
                 images_path=images_path,
-                checkpoint_path=checkpoint_path,
+                save_dir=save_dir,
                 model_name=model_name,
                 max_epochs=max_epochs,
                 lr=lr,
